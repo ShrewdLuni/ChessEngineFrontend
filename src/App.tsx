95a70e9342afcc8005ab7ce6c8e3608e64687232
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-06-26 20:46:24 +0300

    fix: add working tile display

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { ChessBoard } from './components/ChessBoard';
+import pawn from './assets/WhitePawn.png'
 
 function App() {
-  const whiteTile = <div className="h-[100px] bg-[#1a1e23] aspect-square rounded-sm hover:bg-green-600"></div>
-  const blackTile = <div className="h-[100px] bg-[#8c8fbc] aspect-square hover:bg-green-600"></div>
 
-  let board = [];
-  for(let i = 0;i < 64; i++){
-    board.push((i + Math.floor(i/8)) % 2 == 0 ? whiteTile : blackTile)
-  } 
 
   return (
     <div className="flex w-screen h-full min-h-screen bg-[#1a1e23] justify-center items-center">
--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -1,28 +1,46 @@
+import { useEffect, useLayoutEffect } from "react";
+
 export const ChessBoard = () => {
   const whiteTile = <div className="h-[100px] bg-[#1a1e23] aspect-square hover:border-solid hover:border-[#fffaf6] hover:border-[4px] hover:rounded-sm transition-all duration-100"></div>
   const blackTile = <div className="h-[100px] bg-[#8c8fbc] aspect-square hover:border-solid hover:border-[#fffaf6] hover:border-[4px] hover:rounded-sm transition-all duration-100"></div>
+
+  let ChessBoard = document.getElementById("Board");
+  let rect = ChessBoard?.getBoundingClientRect()
+  let x : number = rect == undefined ? -1 : rect.left == undefined ? -1 : rect.left
+  let y : number = rect == undefined ? -1 : rect.top == undefined ? -1 : rect.top
+
+  useLayoutEffect(() => {
+    ChessBoard = document.getElementById("Board");
+    rect = ChessBoard?.getBoundingClientRect()
+    x = rect == undefined ? -1 : rect.left == undefined ? -1 : rect.left
+    y = rect == undefined ? -1 : rect.top == undefined ? -1 : rect.top
+    // ChessBoard?.addEventListener("mousemove", (e) => {
+    //   showPosition(e)
+    // });
+  }, []);
 
   let board = [];
   for(let i = 0;i < 64; i++){
     board.push((i + Math.floor(i/8)) % 2 == 0 ? whiteTile : blackTile)
   } 
 
-  const ChessBoard = document.getElementById("Board");
-  const rect = ChessBoard?.getBoundingClientRect()
-
   let moves = ["a","b","c","d","e","f","g","h"]
 
-  let x : number = rect == undefined ? 0 : rect.left == undefined ? 0 : rect.left
-  let y : number = rect == undefined ? 0 : rect.top == undefined ? 0 : rect.top
-  
-  function showPosition(e : React.MouseEvent<HTMLElement> | MouseEvent) {
-    console.log(moves[Math.floor((e.clientX - x) / 100)] + "" + (8 - Math.floor((e.clientY - y) / 100)))
+  function clamp(target:number, min:number, max:number){
+    if(target < min){
+      return min;
+    }
+    else if(target > max){
+      return max;
+    }
+    return target;
   }
 
-
-  ChessBoard?.addEventListener("mousemove", (e) => {
-    showPosition(e)
-  });
+  function showPosition(e : React.MouseEvent<HTMLElement> | MouseEvent) {
+    let one = clamp(Math.floor((e.clientX - x) / 100), 0, 7)
+    let two = clamp(Math.floor((e.clientY - y) / 100), 0, 7)
+    console.log(moves[one] + "" + (8-two))
+  }
 
   return (
     <div id="Board" className="relative grid grid-rows-8 grid-cols-8 border-[#8c8fbc] border-[4px] aspect-square rounded-sm" onClick={showPosition}>
