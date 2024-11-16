49af13743d435bb0a95c400344a28a99aa3c8b59
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-11-16 22:55:50 +0100

    feat: add no scrollbar class to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -73,5 +73,18 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    function ({ addUtilities }) {
+      addUtilities({
+        '.no-scrollbar': {
+          '-ms-overflow-style': 'none', /* Internet Explorer 10+ */
+          'scrollbar-width': 'none', /* Firefox */
+        },
+        '.no-scrollbar::-webkit-scrollbar': {
+          display: 'none', /* Safari and Chrome */
+        },
+      });
+    },
+  ],
 }
