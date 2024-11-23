aa99f217fb88200dae6c03a7d1d45e6423c4553e
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2024-11-23 22:54:28 +0100

    feat: normalized evaluation value

--- src/components/sidebar/EvaluationBar.tsx
+++ src/components/sidebar/EvaluationBar.tsx
@@ -4,13 +4,16 @@ interface EvaluationBarProps {
   value: number;
 }
 
-export const EvaluationBar= ({ value } : EvaluationBarProps) => {
-  const clampedValue = helpers.clamp(value, 0, 100);
+export const EvaluationBar = ({ value } : EvaluationBarProps) => {
+  const maxValue = 15
+  const normalizedValue = ((value + maxValue) / (2 * maxValue)) * 100;
+  const clampedValue = helpers.clamp(normalizedValue, 0, 100);
+  console.log(clampedValue);
 
   return (
     <div className="h-5 border-black flex">
-      <div className="bg-[#1a1e23] transition-all duration-1000"style={{width: `${clampedValue}%`,}}></div>
-      <div className="bg-[#8c8fbc] transition-all duration-1000" style={{width: `${100 - clampedValue}%`,}}></div>
+      <div className="bg-[#1a1e23] transition-all duration-1000"style={{width: `${100 - clampedValue}%`,}}></div>
+      <div className="bg-[#8c8fbc] transition-all duration-1000" style={{width: `${clampedValue}%`,}}></div>
     </div>
   );
 };
