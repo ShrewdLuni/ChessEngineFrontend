362454f4e2df0f79e160cf4d7a2ec4b444eaeea7
ShrewdLuni <87246480+ShrewdLuni@users.noreply.github.com>
2025-02-26 15:01:17 +0100

    feat: checkmate sound on game over

--- src/hooks/useWebSocket.ts
+++ src/hooks/useWebSocket.ts
@@ -1,4 +1,6 @@
+import { playSound } from "@/lib/utils";
 import { useEffect, useState } from "react";
+import gameover from '../assets/sounds/gameover.mp3'
 
 interface webSocketProps{
   setEvaluation: React.Dispatch<React.SetStateAction<number>>;
@@ -48,6 +50,7 @@ export function useWebSocket({ setEvaluation, setBestMove, setMovesData, updateP
           break;
         case 'engine_game_over':
           setIsGameOver(true);
+          playSound(gameover)
           break;
         case 'engine_set_position':
           updatePiecesFromFEN(data.fen);
